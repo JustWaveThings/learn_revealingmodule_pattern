d09338b593915b5c9dc5b4e026720334248f370b
JustWaveThings <justwavethings@proton.me>
2022-10-02 14:46:27 -0400

    Add people via UI working

--- people.js
+++ people.js
@@ -2,19 +2,41 @@ const people = (function () {
 	let people = ['James', 'Terry'];
 
 	// cache DOM
-	const addButton = document.querySelector('#addPerson');
-	const input = document.querySelector('input');
-	const displayOfPeople = document.querySelector('ul');
-	// create dom elements for names to be rendered
-	const li = document.createElement('li');
-	const span = document.createElement('span');
-	span.textContent = 'addedPerson';
-	const i = document.createElement('i');
-	i.classList.add('del');
-	i.textContent = 'X';
 
-	// render people
-	displayOfPeople.appendChild(li);
-	li.appendChild(i);
-	li.insertBefore(span, i);
+	const addButton = document.querySelector('#addPerson');
+	const inputName = document.querySelector('input');
+	const displayOfPeople = document.querySelector('ul');
+
+	// bind events
+	addButton.addEventListener('click', addPerson);
+
+	// render people initally
+	people.forEach(_render);
+
+	function _render(person) {
+		const li = document.createElement('li');
+		const span = document.createElement('span');
+		const i = document.createElement('i');
+		i.classList.add('del');
+		i.textContent = 'Delete';
+		displayOfPeople.appendChild(li);
+		li.appendChild(i);
+		li.insertBefore(span, i);
+		span.textContent = person;
+	}
+
+	function addPerson() {
+		people.push(inputName.value);
+		console.log(people);
+		_render(people.slice(-1));
+	}
+
+	const deleteButtons = document.querySelectorAll('i');
+	//console.log(deleteButtons);
+	deleteButtons.forEach((button) => {
+		button.addEventListener('click', () => {
+			console.log(`delete button clicked -- ${button.closest('li')}`);
+		});
+	});
+	function deletePerson(e) {}
 })();
